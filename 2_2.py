df94643f5357c7fce3df9315dd23f582333435e5
YutongDu <yutong.du@aalto.fi>
2023-12-01 16:32:59 +0200

    加了一个总图，去掉了x轴，并且改为线型图更好的看趋势

--- 2_2.py
+++ 2_2.py
@@ -19,18 +19,25 @@ df_combined = pd.concat(df_list, ignore_index=True)
 # 计算每个用户（源IP地址）的聚合数据量
 user_data_volume = df_combined.groupby('src')['bytes'].sum().sort_values(ascending=False)
 
-# 将用户分成三个等分
-chunk_size = int(np.ceil(len(user_data_volume) / 3))
-user_chunks = [user_data_volume[i:i + chunk_size] for i in range(0, len(user_data_volume), chunk_size)]
+# # 将用户分成三个等分
+# chunk_size = int(np.ceil(len(user_data_volume) / 3))
+# user_chunks = [user_data_volume[i:i + chunk_size] for i in range(0, len(user_data_volume), chunk_size)]
+#
+# # 为每个部分绘制条形图
+# for i, chunk in enumerate(user_chunks, start=1):
+#     plt.figure(figsize=(12, 6))
+#     chunk.plot(kind='bar', color='skyblue')
+#     plt.title(f'User Aggregated Data Volume - Part {i}')
+#     plt.xlabel('User IP Address')
+#     plt.ylabel('Aggregated Data Volume (bytes)')
+#     plt.xticks(rotation=90)  # Rotate the x labels for better readability
+#     plt.tight_layout()  # Adjust layout to fit IP addresses
+#     plt.yscale('log')
+#     plt.show()
 
-# 为每个部分绘制条形图
-for i, chunk in enumerate(user_chunks, start=1):
-    plt.figure(figsize=(12, 6))
-    chunk.plot(kind='bar', color='skyblue')
-    plt.title(f'User Aggregated Data Volume - Part {i}')
-    plt.xlabel('User IP Address')
-    plt.ylabel('Aggregated Data Volume (bytes)')
-    plt.xticks(rotation=90)  # Rotate the x labels for better readability
-    plt.tight_layout()  # Adjust layout to fit IP addresses
-    plt.yscale('log')
-    plt.show()
+
+plt.figure(figsize=(12, 6))
+user_data_volume.plot(kind='line', color='skyblue')
+plt.title(f'User Aggregated Data Volume')
+plt.ylabel('Aggregated Data Volume (bytes)')
+plt.show()
--- 2_3for2_2.py
+++ 2_3for2_2.py
@@ -13,29 +13,39 @@ df = pd.read_csv(directory_path, sep='\t', header=None, names=columns)
 
 # 计算每个用户（源IP地址）的聚合数据量
 user_data_volume = df.groupby('src')['bytes'].sum().sort_values(ascending=False)
+#
+# # 取前180名用户的数据量
+# top_users = user_data_volume.head(180)
+#
+# # 将其他用户的数据量汇总到 'Other Users'
+# other_users_volume = user_data_volume.iloc[180:].sum()
+# other_users_series = pd.Series([other_users_volume], index=['Other Users'])
+#
+# # 合并 top_users 和 other_users_series
+# top_users_with_others = pd.concat([top_users, other_users_series])
+#
+# # 计算每个图表的大小
+# chunk_size = int(np.ceil(len(top_users_with_others) / 4))
+# user_chunks = [top_users_with_others[i:i + chunk_size] for i in range(0, len(top_users_with_others), chunk_size)]
+#
+# # 为每个部分绘制条形图
+# for i, chunk in enumerate(user_chunks, start=1):
+#     plt.figure(figsize=(12, 6))
+#     chunk.plot(kind='bar', color='skyblue')
+#     plt.title(f'User Aggregated Data Volume - Part {i}')
+#     plt.xlabel('User IP Address')
+#     plt.ylabel('Aggregated Data Volume (bytes)')
+#     plt.xticks(rotation=90)
+#     plt.tight_layout()
+#     plt.yscale('log')
+#     plt.show()
 
-# 取前180名用户的数据量
-top_users = user_data_volume.head(180)
 
-# 将其他用户的数据量汇总到 'Other Users'
-other_users_volume = user_data_volume.iloc[180:].sum()
-other_users_series = pd.Series([other_users_volume], index=['Other Users'])
-
-# 合并 top_users 和 other_users_series
-top_users_with_others = pd.concat([top_users, other_users_series])
-
-# 计算每个图表的大小
-chunk_size = int(np.ceil(len(top_users_with_others) / 4))
-user_chunks = [top_users_with_others[i:i + chunk_size] for i in range(0, len(top_users_with_others), chunk_size)]
-
-# 为每个部分绘制条形图
-for i, chunk in enumerate(user_chunks, start=1):
-    plt.figure(figsize=(12, 6))
-    chunk.plot(kind='bar', color='skyblue')
-    plt.title(f'User Aggregated Data Volume - Part {i}')
-    plt.xlabel('User IP Address')
-    plt.ylabel('Aggregated Data Volume (bytes)')
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.yscale('log')
-    plt.show()
+plt.figure(figsize=(12, 6))
+user_data_volume.plot(kind='line', color='skyblue')
+plt.title(f'User Aggregated Data Volume')
+plt.ylabel('Aggregated Data Volume (bytes)')
+plt.yscale('log')
+ax = plt.gca()
+ax.xaxis.set_visible(False)
+plt.show()
